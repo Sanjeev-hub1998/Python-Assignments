06264fff9313ec5e8125c77e5bbe3da467179f71
Sanjeev Kumar <kumarsanjeevkumar64@gmail.com>
2026-06-20 18:58:33 +0530

    Add files via upload

---
+++ if_questions_with_solution.py
@@ -0,0 +1,74 @@
+"""
+
+1. Write a Python program to check if a number is positive.
+ if 10>5:
+    print("Positive")
+
+---------------------------------------------------------------------
+2. Print "Eligible to vote" if age is 18 or above.
+age = int(input("Enter the age:))
+if age>=18:
+    print("He can vote")
+
+------------------------------------------------------------------------
+3. Check if a number is divisible by 7.
+num = int(input("Enter the number here:))
+if num%7==0:
+    print("Given number can be devided")
+
+-----------------------------------------------------------------------
+4. Print "Pass" if marks are greater than 40.
+
+marks = int(input("Enter your marks:"))
+if marks>40:
+    print("Pass")
+
+------------------------------------------------------------------------
+
+5. Check if a number is greater than 100.
+
+num =int(input("Enter the number:"))
+if num>100:
+    print(num,"is greater than 100")
+
+------------------------------------------------------------------------
+
+6. Display a message if temperature exceeds 45°C.
+
+tem = 48
+if tem>46:
+    print(f"Present temperature is {tem}C\nPlease avoide to go outside because heat is being encontrolled,you may sick if you go outside. ")
+
+---------------------------------------------------------------------------
+
+7. Check if a string length is more than 8 characters. 
+
+str = "SanjeevKumar"
+if len(str)>8:
+    print(f"{str}, having more than 8 characters")
+
+----------------------------------------------------------------------------
+
+8. Print "Logged In" if password matches "admin123". 
+
+entered_pass = input("Enter the password:")
+password = "admin123"
+if entered_pass==password:
+    print("You are logged In")
+
+-----------------------------------------------------------------------------
+
+9. Check if a number is a multiple of 10.
+
+num = 50
+if num%10==0:
+    print(f"{num} is multiple of 10")
+
+--------------------------------------------------------------------------------
+
+10. Print a warning if balance is below minimum limit.
+
+"""
+bal = 80
+if bal<100:
+    print(f"Warning!! Your {bal} should be greater than 100")
